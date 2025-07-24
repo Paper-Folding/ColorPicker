d84fe882c62ee9c3fd919b1c4e37316e74dcdc5e
chenjianbo <specialwave@foxmail.com>
2025-07-24 10:39:45 +0800

    fix: proportion converter got zero division

--- src/ColorPicker/Converters/ProportialConverter.cs
+++ src/ColorPicker/Converters/ProportialConverter.cs
@@ -8,9 +8,9 @@ namespace ColorPicker.Converters
     {
         public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
-            if (values[0] is double && values[1] is double && values[2] is double)
+            if (values[0] is double && values[1] is double && values[2] is double && (double)values[2] != 0.0)
                 return (double)values[0] * ((double)values[1] / (double)values[2]);
-            return 0;
+            return 0.0;
         }
 
         public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
